5c83be223de08f53ce5e0442d8dfafa6997b1283
MINHAO DU <minhaodu@MINHAOdeMacBook-Pro.local>
2018-04-20 21:53:00 +1000

    add about us page

---
+++ about.php
@@ -0,0 +1,218 @@
+<!doctype html>
+<html>
+<head>
+		<meta charset="utf-8">
+		<title>about|SafePath</title>
+		<!-- Mobile Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+		<!-- JavaScript --> 
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/jquery.backstretch.min.js"></script>
+		<!-- function Scripts -->
+		<script type="text/javascript" src="js/function.js"></script>
+		<!-- animation effect -->
+		<link href="css/animate.min.css" rel="stylesheet"> 
+		<script src="js/wow.min.js"></script>
+		<script>
+ 		new WOW().init();
+		</script>
+<!-- //animation effect -->
+<link rel="shortcut icon" href="favicon.ico">
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+	</head>
+
+<body>
+		<!-- header start --> 
+		<header class="header fixed clearfix navbar navbar-fixed-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<!-- header-left start --> 
+						<div class="header-left">
+							<!-- logo -->
+							<div class="logo smooth-scroll">
+								<a href="#banner"><img id="logo" src="images/logo3.png" alt="Worthy"></a>
+							</div>
+							<!-- name-and-slogan -->
+							<div class="logo-section smooth-scroll">
+								<div class="brand"><a href="#banner">SAFE PATH</a></div>								
+							</div>
+						</div>
+						<!-- header-left end -->
+					</div>
+					<div class="col-md-8">
+						<!-- header-right start --> 
+						<div class="header-right">
+							<!-- main-navigation start --> 
+							<div class="main-navigation animated">
+								<!-- navbar start --> 
+								<nav class="navbar navbar-default" role="navigation">
+									<div class="container-fluid">
+										<!-- Toggle get grouped for better mobile display -->
+										<div class="navbar-header">
+											<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+												<span class="sr-only">Toggle navigation</span>
+												<span class="icon-bar"></span>
+												<span class="icon-bar"></span>
+												<span class="icon-bar"></span>
+											</button>
+										</div>
+										<!-- Collect the nav links, forms, and other content for toggling -->
+										<div class="collapse navbar-collapse scrollspy smooth-scroll" id="navbar-collapse-1">
+											<ul class="nav navbar-nav navbar-right">
+                                                <li><a href="index.php">Home</a></li>
+												<li><a href="Events/index.php">Events</a></li>
+												<li><a  style="color:#ffc400;" href="about.php">About Us</a></li>
+											</ul>
+										</div>
+									</div>
+								</nav>
+								<!-- navbar end -->
+							</div>
+							<!-- main-navigation end -->
+						</div>
+						<!-- header-right end -->
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- header end -->
+		<!-- banner start --> 
+		<div class="banner1"></div>
+		<!-- banner end -->
+<div class="about" style="background:#eee;">
+        <div class="container">
+            <div class="section-header">
+                <h2 class="section-title text-center wow fadeInDown">About Us</h2>
+                <p class=" about-us text-center wow fadeInDown">The Sympatico team came together in Monash IE 2018, Semester 1. The members bring together diverse cultural, linguistic, and technical skills into an environment of cooperation and collaboration. The team consists of two Master of Information Technology students and three Master of Business Information Systems students. Our topic theme is "Wow! I've seen..." and our solution is SafePath.</p>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-6 wow">
+                  <img class="img-responsive" src="images/banner.jpg" alt="">
+                </div>
+                <div class="col-sm-6 wow" style="background: #FFFFFF;">
+                    <h3 class="column-title">About SafePath</h3>
+                    <p>SafePath helps people get to or from events in a safer way. Users can select an event, view the different transport alternatives, and view real-time reports from other users.</p>
+
+                    <p>SafePath users observe and log hazards and concerns, such as aggressive groups or overcrowding, when at an event, or in transit. Other users can take this contextual information and use it to make better safety choices. Users can also log positive information about the event or route such as quick service or wide range of food. Next time you head to an event make sure to take the SafePath.</p>
+
+                </div>
+            </div>
+        </div>
+		<div class="team-section">
+				<div class="container">
+					<div class="section-header">
+					<h2 class="section-title text-center wow fadeInDown">Our Team Members</h2>
+					</div>
+					<div class="team-grids">
+						<div class="col-md-4 team-grid wow fadeInLeft animated animated" data-wow-delay="0.4s">
+							<div class="ih-item circle effect1"><a href="#">
+								<div class="spinner"></div>
+								<div class="img"><img src="images/Miki_ID.jpg" alt="img"></div>
+								<div class="info">
+								  <div class="info-back">
+									<h4>Mikaela Jennings</h4>
+									<span>Data sourcing</span>
+								  </div>
+								</div></a>
+							</div>
+						</div>
+						<div class="col-md-4 team-grid wow fadeInDownBig animated animated" data-wow-delay="0.4s">
+							<div class="ih-item circle effect1"><a href="#">
+								<div class="spinner"></div>
+									<div class="img"><img src="images/yunshu.jpg" alt="img"></div>
+								<div class="info">
+									<div class="info-back">
+										<h4>Yunshu Zhao</h4>
+										<span>Database design</span>
+								  </div>
+								</div></a>
+							</div>
+						</div>
+						<div class="col-md-4 team-grid wow fadeInRight animated animated" data-wow-delay="0.4s">
+							<div class="ih-item circle effect1"><a href="#">
+								<div class="spinner"></div>
+									<div class="img"><img src="images/sherry.jpg" alt="img"></div>
+								<div class="info">
+									<div class="info-back">
+										<h4>Sherry Xiang</h4>
+										<span>UI designer</span>
+								  </div>
+								</div></a>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+	                <div class="team-grids">
+					<div class="col-md-6 team-grid wow fadeInLeft animated animated" data-wow-delay="0.4s">
+							<div class="ih-item circle effect1"><a href="#">
+								<div class="spinner"></div>
+									<div class="img"><img src="images/Solene_id.jpg" alt="img"></div>
+								<div class="info">
+									<div class="info-back">
+										<h4>Solène Greliche</h4>
+										<span>Coding of Back-end</span>
+								  </div>
+								</div></a>
+							</div>
+						</div>
+						<div class="col-md-6 team-grid wow fadeInRight animated animated" data-wow-delay="0.4s">
+							<div class="ih-item circle effect1"><a href="#">
+								<div class="spinner"></div>
+									<div class="img"><img src="images/Minhao.jpg" alt="img"></div>
+								<div class="info">
+									<div class="info-back">
+										<h4>MinHao Du</h4>
+										<span>Coding of front-end</span>
+								  </div>
+								</div></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+    </div>
+
+			<!---team--->
+
+
+	<!--/#about-->
+
+<!-- footer start --> 
+		<footer id="footer">
+			<div class="subfooter">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<p class="text-center">&copy; 2018.Sympatico</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- footer end -->
+		<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+				
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+</body>
+</html>
