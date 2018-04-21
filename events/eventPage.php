b1d4ce233da47880217080f0f859505b19a2d7ea
Solene <sgre29@student.monash.edu>
2018-04-21 17:36:03 +1000

    Change eventPage.php add bar chart and update when change data

--- events/eventPage.php
+++ events/eventPage.php
@@ -7,6 +7,11 @@
   <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.js"></script>
    <link href="css/test.css" rel="stylesheet">
+   <link href="../css/style.css" rel="stylesheet">
+   <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="../css/bootstrap.css" rel="stylesheet"> 
+  <link href="../css/animate.min.css" rel="stylesheet">
    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
    
 <style>
@@ -67,11 +72,108 @@ function UrlExists(url)
 
 </script>
 
+<body>
+
+<!-- header start --> 
+    <!-- header start --> 
+    <header class="header fixed clearfix navbar navbar-fixed-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <!-- header-left start --> 
+            <div class="header-left">
+              <!-- logo -->
+              <div class="logo smooth-scroll">
+                <a href="#banner"><img id="logo" src="../images/logo3.png" alt="Worthy"></a>
+              </div>
+              <!-- name-and-slogan -->
+              <div class="logo-section smooth-scroll">
+                <div class="brand"><a href="#banner">SAFE PATH</a></div>                
+              </div>
+            </div>
+            <!-- header-left end -->
+          </div>
+          <div class="col-md-8">
+            <!-- header-right start --> 
+            <div class="header-right">
+              <!-- main-navigation start --> 
+              <div class="main-navigation animated">
+                <!-- navbar start --> 
+                <nav class="navbar navbar-default" role="navigation">
+                  <div class="container-fluid">
+                    <!-- Toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                      </button>
+                    </div>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse scrollspy smooth-scroll" id="navbar-collapse-1">
+                      <ul class="nav navbar-nav navbar-right">
+                                                <li><a href="index.php">Home</a></li>
+                        <li><a href="index.php">Events</a></li>
+                        <li><a  style="color:#ffc400;" href="about.php">About Us</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </nav>
+                <!-- navbar end -->
+              </div>
+              <!-- main-navigation end -->
+            </div>
+            <!-- header-right end -->
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- header end -->
+    <!-- banner start --> 
+    <div class="banner1"></div>
+    <!-- banner end -->
 
 
 </head>
 
 <body>
+<form method="get" action="parameters.html"> </form>
+
+
+<script language="JavaScript">
+
+  var eventId;
+
+  function getEventId() {
+    eventId =  unescape(location.search.substring(1).split("&")[0]);
+  }
+  
+  getEventId();
+
+  console.log(eventId);
+
+  function getEventById() {
+
+    var token = 'BY5GBM6TSQPL3NTR5S6E';
+
+    $.get('https://www.eventbriteapi.com/v3/events/' + eventId + '/?token=' + token + '&expand=venue', function(event) {
+
+        console.dir(event);
+
+                document.getElementById("eventName").innerHTML = event.name.text;
+                document.getElementById("eventUrl").innerHTML = "<a href='" + event.url + "' target=\"_blank\" style=\";font-weight:700;\">Click here for event page</a>";
+                document.getElementById("eventDate").innerHTML = moment(event.start.local).format('D/M/YYYY h:mm A');
+                document.getElementById("eventDescription").innerHTML = event.description.html;
+        });
+                  
+  };
+
+
+  getEventById();
+
+</script>
+
 
   <!-- Get Camera, taxi ranks and Police stations data from database -->
 
@@ -106,10 +208,10 @@ function UrlExists(url)
   <div id="eventUrl" style="float: left; width: 50%;" align="center"></div>
 </div>
 
-<hr width="100%";>  
+</br></br><hr width="100%";></br>  
 
 <div style="content: ""; display: table; clear: both;">
-  <div id="directionsPanel" style="float: left; width: 30%;" align="left">
+  <div id="directionsPanel" style="float: left; width: 30%; padding-left:25px;" align="left">
     <b>Choose starting point:</b>    
         <input id="origin-input" class="controls" type="text" style ="width:250px" placeholder="Enter an origin location"></br></br>
           <b>Transport type: </b>
@@ -127,11 +229,12 @@ function UrlExists(url)
             <div id="panel" style="width: 90%;"></div>
           </div>
   </div>
-  <div id="mapAndInfoPanel" style="float: left; width: 70%;" align="center">
+  <div id="mapAndInfoPanel" style="float: left; width: 70%; padding-right: 25px" align="left">
     <div class="horizontalTab">
-  <button class="tablinks" id="defaultOpen" onclick="openMapOrInfo(event, 'safety')">Safety Features</button>
-  <button class="tablinks" onclick="openMapOrInfo(event, 'event')">Event Info</button>
-  <button class="tablinks" onclick="openMapOrInfo(event, 'route')">Route Updates</button>
+  <button class="tablinks" onclick="openMapOrInfo(event, 'route')">Direction to event</button>
+  <button class="tablinks" onclick="openMapOrInfo(event, 'safety')">Safety Features</button>
+  <button class="tablinks" onclick="openMapOrInfo(event, 'route')">Route Hazards</button>
+  <button class="tablinks" id="defaultOpen" onclick="openMapOrInfo(event, 'event')">Event Feedback</button>
   </div>
     
   <div id="mapPanel" class="mapOrInfoPanelContent">
@@ -164,34 +267,99 @@ function UrlExists(url)
 
   <div id="phpOutput"></div>
 
-  <button onclick="setValue('noToilets');">No Toilets</button>
-  <button onclick="setValue('longQueue');">Long Queues</button>
-  <button onclick="setValue('foodRange');">Wide Range of Food</button>
-  <button onclick="setValue('quickService');">Quick Service</button></br>
-
-  <button onclick="getUserData();">Show event data</button></br></br>
-
-  <button onclick="barChart();">Bar Chart</button></br></br>
+  <p>What would you like to report?</p>
+  <button class="choice" onclick="setValue('noToilets');">No Toilets</button>
+  <button class="choice" onclick="setValue('longQueue');" >Long Queues</button>
+  <button class="choice" onclick="setValue('foodRange');" >Wide Range of Food</button>
+  <button class="choice" onclick="setValue('quickService');" >Quick Service</button></br>
 
 
 
-
-  <div id="userData" style="display: none;"></div>
-  <div id="ipData" style="display: none;"></div>
+  <div id="userData" style="display: none;">[{"description": "foodRange", "total": 0},{"description":"longQueue","total":0}, {"description":"noToilets","total":0}, {"description":"quickService","total":0}]</div>
+  <div id="ipData" style="display: none;">"[{totalIP: 0}]"</div>
   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
   <div id="chartDiv"></div>
 
   <script>
 
+  
+
+function switchNames(codeName) {
+  var out = '';
+   switch (codeName) {
+        case 'foodRange':
+            out = 'Wide range of food';
+            break;
+        case 'longQueue':
+            out = 'Long queues';
+            break;
+        case 'noToilets':
+            out = 'No toilets';
+            break;
+        case 'quickService':
+            out = 'Quick service';
+            break;
+    }
+    return out;
+}
+
+function switchColours(codeName) {
+  var out = '';
+   switch (codeName) {
+        case 'foodRange':
+            out = 'green';
+            break;
+        case 'longQueue':
+            out = 'red';
+            break;
+        case 'noToilets':
+            out = 'red';
+            break;
+        case 'quickService':
+            out = 'green';
+            break;
+    }
+    return out;
+}
+
+
+
+
   function barChart() {
   var userData = JSON.parse(document.getElementById('userData').innerHTML);
   var ipData = JSON.parse(document.getElementById('ipData').innerHTML);
 
+
+  function addZeroCounts(){
+  var allDescriptions = ["foodRange", "longQueue", "noToilets", "quickService"];
+  var currentDescriptions = [];
+
+
+  for(var i = 0; i < userData.length; i++) {
+    currentDescriptions.push(userData[i].description);
+  }
+
+
+  for(var i = 0; i < allDescriptions.length; i++) {
+    if(currentDescriptions.indexOf(allDescriptions[i])<0){
+      userData.push({description: allDescriptions[i], total:0});
+      console.dir(allDescriptions[i]);
+    }
+  }
+
+
+  }
+
+
+  addZeroCounts();
+    console.dir(userData);
+
+
   var dataTable = new Array();
-  dataTable[0] = ['Description', 'Total'];
+  dataTable[0] = ['Description', 'Total', { role: 'style' }];
    for(var i=0; i<userData.length;i++) {
-    console.log(userData[i]);
-    dataTable[i+1] = [ userData[i].description , userData[i].total ];
+    /*console.log(switchNames(userData[i].description));*/
+    dataTable[i+1] = [ switchNames(userData[i].description) , userData[i].total,  switchColours(userData[i].description) ];
    }
 
    console.log(dataTable);
@@ -205,7 +373,8 @@ function drawBasic() {
       var data = google.visualization.arrayToDataTable(dataTable,false);
 
       var options = {
-        title: 'Population of Largest U.S. Cities',
+        title: '',
+        legend: 'none',
         chartArea: {width: '50%'},
         hAxis: {
           viewWindowMode:'explicit',
@@ -214,7 +383,7 @@ function drawBasic() {
           maxValue: ipData[0].totalIP
         },
         vAxis: {
-          title: 'City'
+          title: ''
         }
       };
 
@@ -228,6 +397,9 @@ function drawBasic() {
 
   }
   
+
+
+
   </script>
 
 
@@ -242,6 +414,12 @@ function getUserData(){
         $("#phpOutput").load("php/getUserData.php?eventIdValue="+eventId);
 
     };
+
+    getUserData();
+
+    barChart();
+
+    var intervalID = setInterval(function(){getUserData(); barChart();}, 1000);
 </script>
 
 
@@ -264,38 +442,7 @@ function getUserData(){
 
 
 
-<script language="JavaScript">
 
-	var eventId;
-
-	function getEventId() {
-		eventId =  unescape(location.search.substring(1).split("&")[0]);
-	}
-	
-	getEventId();
-
-	console.log(eventId);
-
-	function getEventById() {
-
-		var token = 'BY5GBM6TSQPL3NTR5S6E';
-
-		$.get('https://www.eventbriteapi.com/v3/events/' + eventId + '/?token=' + token + '&expand=venue', function(event) {
-
-				console.dir(event);
-
-                document.getElementById("eventName").innerHTML = event.name.text;
-                document.getElementById("eventUrl").innerHTML = "<a href='" + event.url + "' target=\"_blank\" style=\";font-weight:700;\">Click here for event page</a>";
-                document.getElementById("eventDate").innerHTML = moment(event.start.local).format('D/M/YYYY h:mm A');
-                document.getElementById("eventDescription").innerHTML = event.description.html;
-        });
-        					
-	};
-
-
-	getEventById();
-
-</script>
 
 
 <script>
@@ -346,6 +493,6 @@ document.getElementById("defaultOpen").click();
 </script>
 
 
-<form method="get" action="parameters.html"> </form>
+
 </body>
 </html>
