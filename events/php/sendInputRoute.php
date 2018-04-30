ecb70b005defe0a0d7f507fd78c0acb511c6017e
Solene <sgre29@student.monash.edu>
2018-05-01 00:33:15 +1000

    create sendInputRoute.js to log user input data to database

---
+++ events/php/sendInputRoute.php
@@ -0,0 +1,40 @@
+
+<?php
+
+
+function sendInput(){
+
+$servername = "localhost";
+$username = "client";
+$password = "password";
+$dbname = "safepath";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$IP = $_SERVER['REMOTE_ADDR'];
+
+$sql = "INSERT INTO pinpoint(eventID, description, userip, lat, lon) values('" .  $_GET["eventIdValue"] . "', '" . $_GET["description"] . "' , '" . $IP . "')";
+
+if ($conn->query($sql) === TRUE) {
+   // echo "New record created successfully";
+    echo '<script language="javascript">';
+	echo 'alert("Your input has been recorded")';
+	echo '</script>';
+} else {
+        echo '<script language="javascript">';
+	echo 'alert("Your input has already been recorded")';
+	echo '</script>';
+}
+
+$conn->close();
+}
+
+
+sendInput();
+
+?> 
